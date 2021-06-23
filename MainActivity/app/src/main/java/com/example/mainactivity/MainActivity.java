dac95a9fb6ca2ebaa66cec0adc43431ab3eb7975
TJ <leex9121@umn.edu>
2021-06-23 15:43:31 +0900

    uploading noticelist

--- MainActivity/app/src/main/java/com/example/mainactivity/MainActivity.java
+++ MainActivity/app/src/main/java/com/example/mainactivity/MainActivity.java
@@ -1,14 +1,147 @@
 package com.example.mainactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView noticeListView;
+    private NoticeListAdapter adapter;
+    private List<Notice> noticeList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        noticeListView = (ListView) findViewById(R.id.noticeListView);
+        noticeList = new ArrayList<Notice>();
+        adapter = new NoticeListAdapter(getApplicationContext(), noticeList);
+        noticeListView.setAdapter(adapter);
+
+        final Button mokjangButton = (Button) findViewById(R.id.mokjangButton);
+        final Button rideButton = (Button) findViewById(R.id.rideButton);
+        final Button speechButton = (Button) findViewById(R.id.speechButton);
+        final LinearLayout notice = (LinearLayout) findViewById(R.id.notice);
+
+        mokjangButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                notice.setVisibility(View.GONE);
+                mokjangButton.setBackgroundColor(getResources().getColor(R.color.colorGray));
+                rideButton.setBackgroundColor(getResources().getColor(R.color.design_default_color_primary));
+                speechButton.setBackgroundColor(getResources().getColor(R.color.design_default_color_primary));
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment, new MokjangFragment());
+            }
+        });
+
+        rideButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                notice.setVisibility(View.GONE);
+                mokjangButton.setBackgroundColor(getResources().getColor(R.color.design_default_color_primary));
+                rideButton.setBackgroundColor(getResources().getColor(R.color.colorGray));
+                speechButton.setBackgroundColor(getResources().getColor(R.color.design_default_color_primary));
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment, new RideFragment());
+            }
+        });
+
+        speechButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                notice.setVisibility(View.GONE);
+                mokjangButton.setBackgroundColor(getResources().getColor(R.color.design_default_color_primary));
+                rideButton.setBackgroundColor(getResources().getColor(R.color.design_default_color_primary));
+                speechButton.setBackgroundColor(getResources().getColor(R.color.colorGray));
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment, new SpeechFragment());
+            }
+        });
+
+        new BackgroundTask().execute();
+    }
+
+    class BackgroundTask extends AsyncTask<Void, Void, String> {
+        String target;
+
+        @Override
+        protected void onPreExecute() {
+            target = "https://xown17.cafe24.com/NoticeList.php";
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            try {
+                URL url = new URL(target);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                String temp;
+                StringBuilder stringBuilder = new StringBuilder();
+                while((temp = bufferedReader.readLine()) != null) {
+                    stringBuilder.append(temp + "\n");
+                }
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+                return stringBuilder.toString().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        public void onProgressUpdate(Void... values) {
+            super.onProgressUpdate();
+        }
+
+        @Override
+        public void onPostExecute(String result) {
+            try {
+                JSONObject jsonObject = new JSONObject(result);
+                JSONArray jsonArray = jsonObject.getJSONArray("response");
+                int count = 0;
+                String noticeContent, noticeName, noticeDate;
+                while(count < jsonArray.length()) {
+                    JSONObject object = jsonArray.getJSONObject(count);
+                    noticeContent = object.getString("noticeContent");
+                    noticeName = object.getString("noticeName");
+                    noticeDate = object.getString("noticeDate");
+                    Notice notice = new Notice(noticeContent, noticeName, noticeDate);
+                    noticeList.add(notice);
+                    adapter.notifyDataSetChanged();
+                    count++;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
